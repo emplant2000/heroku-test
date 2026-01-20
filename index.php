e8d363766994445a0aceff33e041254461946e6d
emplant2000 <emplant2000@gmail.com>
2026-01-21 05:36:51 +0900

    Add API key validation script to index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?phpinfo();?>
+<html>
+<script>
+const apiKey = "x1mwcyc2r0du3ripbckdmstr11l6fb36bx6bdgpryerirgi0indnzjpdummnrpog";
+
+fetch("https://api.minepi.com/v2/me", {
+  headers: { Authorization: "Key " + apiKey }
+})
+  .then(res => {
+    console.log(res.status === 200 ? "API key OK" : "API key NG (" + res.status + ")");
+  })
+  .catch(err => console.error("Error:", err));
+</script>
+  
+</html>
