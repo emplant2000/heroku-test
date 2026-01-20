6db08aae84a7892ab79b2601638e430cd2e46cd6
emplant2000 <emplant2000@gmail.com>
2026-01-21 05:44:25 +0900

    Replace JavaScript API call with PHP cURL request

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?phpinfo();?>
-<html>
-<script>
-const apiKey = "x1mwcyc2r0du3ripbckdmstr11l6fb36bx6bdgpryerirgi0indnzjpdummnrpog";
 
-fetch("https://api.minepi.com/v2/me", {
-  headers: { Authorization: "Key " + apiKey }
-})
-  .then(res => {
-    console.log(res.status === 200 ? "API key OK" : "API key NG (" + res.status + ")");
-  })
-  .catch(err => console.error("Error:", err));
-</script>
-  
-</html>
+<?php
+// 最短・素手 cURL 版（Pi Server に投げて 200 が返れば OK とみなす）
+
+$apiKey = 'x1mwcyc2r0du3ripbckdmstr11l6fb36bx6bdgpryerirgi0indnzjpdummnrpog';
+
+$ch = curl_init('https://api.minepi.com/v2/me');
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER     => ['Authorization: Key ' . $apiKey],
+]);
+$res  = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo $code === 200 ? 'API key OK' : 'API key NG (HTTP ' . $code . ')';
